e954284fd5c7dfcebd94e992961844e4e51ac7c9
OnalennaRammapatsi <othandekamotlanye@gmail.com>
2023-03-28 14:48:51 +0200

    IWA2

---
+++ IWA2/JS/challenge2.js
@@ -0,0 +1,9 @@
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account
+ */
+const date = '10/07/2014';
+
+// Log the value of the date variable to the console
+console.log(date);
---
+++ IWA2/JS/challenge3.js
@@ -0,0 +1,11 @@
+
+// It is important to show the following message in order to:
+// - assure users
+// - scare hackers
+// - impress investors
+
+console.log('Security scan starting');
+
+/* It is important to let users know when they can close the page */
+
+console.info('Please wait for scan to complete before closing the browser.');
