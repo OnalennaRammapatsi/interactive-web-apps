15249cc194e16f09f1c852f74742bd7647f335dc
OnalennaRammapatsi <othandekamotlanye@gmail.com>
2023-03-29 20:34:52 +0200

    challenge2

--- IWA3/challenge2/Nwabisa.js
+++ IWA3/challenge2/Nwabisa.js
@@ -3,5 +3,5 @@ const firstname = "Nwabisa";
 const surname = "Gabe";
 export const role3 = "CEO";
 
-const display= firstname + " " + surname + " (" + role3 + ")"
-document.querySelector('#nwabisa').innerText = display
+const display= firstname + " " + surname + " (" + role3 + ")";
+document.querySelector('#nwabisa').innerText = display;
