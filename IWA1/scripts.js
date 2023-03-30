9246df0b3b083b51bab845ffb94d9126f168feb9
OnalennaRammapatsi <othandekamotlanye@gmail.com>
2023-03-30 23:06:46 +0200

    IWA1

---
+++ IWA1/scripts.js
@@ -0,0 +1,5 @@
+const number = document.querySelector('[data-key="number"]')
+const substract = document.querySelector('[data-key="substract"]')
+const add = document.querySelector('[data-key="add"]')
+
+console.log(number,substract,add)
